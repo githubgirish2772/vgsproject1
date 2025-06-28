ea3a1594d9b665ef9aa8a04fd8145fdd8f7ddab3
githubgirish2772 <55174702+githubgirish2772@users.noreply.github.com>
2025-06-28 13:36:38 +0530

    Bhargavi
    This flle provides implementation to calculate area, volume and lateral area of cube

---
+++ cube.h
@@ -0,0 +1,18 @@
+float volume_cube(int a)
+{
+	int res;
+	res= a*a*a;
+	return res;
+}
+float area_cube(int a)
+{
+	int res;
+	res=6*a*a;
+	return res;
+}
+float lateralarea_cube(int a)
+{
+	int res;
+	res=4*a*a;
+	return res;
+}
