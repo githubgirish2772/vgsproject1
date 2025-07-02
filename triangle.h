803621ce6791b028231dc7ab3ee39ca307b24f90
githubgirish2772 <55174702+githubgirish2772@users.noreply.github.com>
2025-07-02 10:51:06 +0530

    Sahithi
    This file implements the functionality to calculate area and perimeter of triangle

---
+++ triangle.h
@@ -0,0 +1,12 @@
+float area(int b,int h)
+{
+	float area;
+	area=1/2 * b * h;
+	return area;
+}
+int perimeter(int l,int b,int h)
+{
+	int perimeter;
+	perirmeter = l + b + h;
+	return perimeter;
+}
